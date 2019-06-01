d00b128e863c6ff1879ad63cb0f23678f6a5a187
Emilio Wuerges <wuerges@gmail.com>
2019-06-01 13:46:20 -0300

    Added parser library

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://dl.bintray.com/hotkeytlt/maven") }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    compile("com.github.h0tk3y.betterParse:better-parse-jvm:0.4.0-alpha-3")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/CircuitParser.kt
@@ -0,0 +1,2 @@
+object CircuitParser {
+}
