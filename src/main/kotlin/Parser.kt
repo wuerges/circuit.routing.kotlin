1c9ae3ec07e4f2934b446ad818969c33b43045be
Emilio Wuerges <wuerges@gmail.com>
2019-06-01 19:19:21 -0300

    base case ok

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -1,7 +1,18 @@
-sealed class Parser
+import java.lang.Exception
 
-data class Partial(val i : Int, val text : String) : Parser()
-data class Error(val i : Int, val text : String, val error : String) : Parser()
+class Error(val i : Int, val text : String, error : String) : Exception(error)
+
+class Parser(val i : Int, val text : String) {
+    fun fail(message :String) : Error {
+        return Error(i, text, message)
+    }
+
+    override fun toString() : String {
+        val a = text.substring(0, i)
+        val b = text.substring(i)
+        return "Parser{$i, \"$a\", \"$b\"}"
+    }
+}
 
 sealed class Either<A, B>
 class Left<A, B>(val left: A) : Either<A, B>()
@@ -22,45 +33,56 @@ class Map<T1, T2>(val grammar: Grammar<T1>, val f :(p: T1) -> T2 ) : Grammar<T2>
 class Or<T1, T2>(val g1: Grammar<T1>, val g2: Grammar<T2>) : Grammar<Either<T1, T2>>()
 {
     override fun check(input: Parser) : Pair<Either<T1, T2>, Parser>{
-        val (a1, b1) = g1.check(input)
-        when(b1) {
-            is Partial -> {
-                return Pair(Left(a1), b1)
-            }
+
+        try {
+            val (a1, b1) = g1.check(input)
+            return Pair(Left(a1), b1)
         }
-        val (a2, b2) = g2.check(input)
-        when(b2) {
-            is Partial -> {
+        catch (e : Error) {
+            try {
+                val (a2, b2) = g2.check(input)
                 return Pair(Right(a2), b2)
             }
+            catch(e2 : Error) {
+                throw input.fail(e.message + " <+or+> " + e2.message)
+            }
         }
-        return Pair(Left(a1), b2)
     }
 }
+
+class And<T1, T2>(val g1: Grammar<T1>, val g2: Grammar<T2>) : Grammar<Pair<T1, T2>>()
+{
+    override fun check(input: Parser) : Pair<Pair<T1, T2>, Parser>{
+        val (a1, b1) = g1.check(input)
+        val (a2, b2) = g2.check(b1)
+        return Pair(Pair(a1, a2), b2)
+    }
+}
+
+
+
 
 class Token(val text : String) : Grammar<String>() {
     val expr = text.toRegex()
 
     override fun check(input : Parser) : Pair<String, Parser> {
-        when (input) {
-            is Partial -> {
-                val res = expr.find(input.text, input.i)
-                if(res != null) {
-                    return Pair(res.value, Partial(input.i+res.value.length, input.text))
-                }
-                else {
-                    return Pair("", Error(input.i, input.text, "Could not match $text"))
-                }
-            }
+        val res = expr.find(input.text, input.i)
+        if(res != null) {
+            return Pair(res.value, Parser(input.i+res.value.length, input.text))
         }
-        return Pair("", input)
+        throw input.fail("Could not match $text")
     }
 }
 
 
 fun main() {
-    val x = Partial(0, "123 mundo")
+    val x = Parser(0, "123 456")
+
+    val ws = Token("\\s*")
 
     val t = Map(Token("\\d+"), { it.toInt() })
-    println(t.check(x))
+    val t2 = And(t, And(ws, t))
+
+
+    println(t2.check(x))
 }
